e2b0dbf68a03fa129b8a0a497521fb23eb8e38c3
Melusine <melusine.liebaut@gmail.com>
2023-03-17 19:01:53 +0100

    sendmail setup + smtp + mail verif

--- backend/register.php
+++ backend/register.php
@@ -1,6 +1,9 @@
 <?php  
 
-require_once './index.php';
+declare(strict_types=1);
+use Firebase\JWT\JWT;
+require_once('./vendor/autoload.php');
+require_once ('./index.php');
 
 // Table Login
 $email = $_POST["email"];
@@ -18,6 +21,8 @@ var_dump($_POST);
 
 if(empty($email) || empty($password) || empty($name) || empty($surname) || empty($dob) || empty($address)) {
    echo "</br> Un ou plusieurs champs est manquant";
+
+   exit(0);
 }
 
 
@@ -59,4 +64,41 @@ try {
   }
 
 echo "</br> Profile cree";
+
+// Creation du JWT
+
+$secretKey  = 'bGS6lzFqvvSQ8ALbOxatm7/Vk7mLQyzqaS34Q4oR1ew=';
+$issuedAt   = new DateTimeImmutable();
+$expire     = $issuedAt->modify('+60 minutes')->getTimestamp();      // Ajoute 60 secondes
+$serverName = $_SERVER['SERVER_NAME'];                                        // Récupéré à partir des données POST filtré
+
+$data = [
+'iat'  => $issuedAt->getTimestamp(),         // Issued at:  : heure à laquelle le jeton a été généré
+'iss'  => $serverName,                       // Émetteur
+'nbf'  => $issuedAt->getTimestamp(),         // Pas avant..
+'exp'  => $expire,                           // Expiration
+'email' => $email,                     // Nom d'utilisateur
+];
+
+//Encoder le tableau en une chaine JWT.
+
+$jwt = JWT::encode(
+    $data,
+    $secretKey,
+    'HS512'
+);
+
+$link = 'http://localhost:3000/verify-account/'. $jwt;
+
+// Envoie du mail de verification du compte
+
+
+if (mail($email, "Verification du compte", $link, "From: propane_nightmare@live.fr")) {
+  echo "</br> Email envoyé avec succès";
+} else {
+  echo "</br> Échec de l'envoi de l'email...";
+}
+
+
+
 
