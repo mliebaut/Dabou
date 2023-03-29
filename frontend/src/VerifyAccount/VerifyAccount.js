61e5b66d9619296469eb22a56f03a90cd92c3dfb
corentindelaunay <75681088+CorentinDel@users.noreply.github.com>
2023-03-29 21:38:18 +0200

    Add verify-account page

--- frontend/src/VerifyAccount/VerifyAccount.js
+++ frontend/src/VerifyAccount/VerifyAccount.js
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import axios from 'axios';
 
 const VerifyAccount = () => {
-
     let {id} = useParams();
+    const [responseType, setResponseType] = useState("");
+
+    useEffect(() => {
+        axios.put(`http://localhost:3000/verify-account`, {verifyToken : id}).then(response => {
+            setResponseType(response.code || "no response");
+        });
+    }, [responseType]);
 
     return (
-        <h1>Page Verification de compte: {id}</h1>
+        <>
+            <h1>Page Verification de compte: {id}</h1>
+            <span>{responseType}</span>
+        </>
     );
 }
 
